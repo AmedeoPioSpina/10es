1dd7762c6847fc268b2db4c62fda02fea65209ee
RoninMirrorImage <spina.amedeo.pio@gmail.com>
2022-07-16 20:43:59 +0200

    es5 finito

--- index.js
+++ index.js
@@ -1,12 +1,69 @@
-// Es 1
+// Es 1 ------------------------------------------------------------------------------------------------------------------------------------
+
+// let even;
 
 // for(let i = 0; i<=10; i++){
-//     if(i%2===0){
-//         console.log(i);
+//     even = i;
+//     if(even%2===0){
+//         console.log(even);
 //     }
 //     else{}
 // }
 
 
-// Es 2
+// Es 2 ------------------------------------------------------------------------------------------------------------------------------------
 
+// let array = [];
+// const MAXMULTIPLIDI= 10;
+
+// for(let i = 1; i<=MAXMULTIPLIDI; i++){
+//     array.push([]);
+//     for(let j = 1; j<=MAXMULTIPLIDI; j++){
+//         array[i-1].push(i*j);
+//     };
+// };
+
+// console.table(array);
+
+
+// Es 3 ------------------------------------------------------------------------------------------------------------------------------------
+
+// const kmMilesConverter = function(){
+    
+//     const oneMiles = 1.609;
+    
+//     const kmInput = prompt("Inserire KM da convertire in Mils");
+//     const milsOutput = (kmInput / oneMiles);
+//     console.log(milsOutput + " Mils");
+// }();
+
+// Es 4 ------------------------------------------------------------------------------------------------------------------------------------
+
+// const array = []
+// let arySum = 0;
+
+// for(let i = 0; i<10; i++){
+//     array.push(parseInt(Math.random() * 100));
+// };
+
+// array.forEach(element => {
+//     arySum += element;
+// });
+
+// console.log(array);
+// console.log(arySum);
+
+// Es 5 ------------------------------------------------------------------------------------------------------------------------------------
+
+// const array = [];
+
+// for(let i = 0; i<5; i++){
+//     array.push(parseInt(Math.random() * 100));
+// };
+
+// const aryCopy = [...array];
+// const reverseAry = aryCopy.reverse();
+// console.log(array);
+// console.log(reverseAry);
+
+// Es 6 ------------------------------------------------------------------------------------------------------------------------------------
